3381f8c4ccf487b8c0f3a6d8a6010560a2a3842d
haozi <syntaxright@gmail.com>
2019-12-07 15:51:01 +0800

    feat: 🎸 custom outDir, add --mode args

--- src/rollup.config.ts
+++ src/rollup.config.ts
@@ -1,17 +1,24 @@
 import * as uglify from 'rollup-plugin-uglify-es'
 import * as _typescript2 from 'rollup-plugin-typescript2'
-
+import * as _replace from '@rollup/plugin-replace'
 const typescript2: any = _typescript2
+const replace: any = _replace
 interface IOptions {
   input: string
+  outDir: string
   format: 'esm' | 'cjs' | 'amd' | 'commonjs' | 'es' | 'iife' | 'module' | 'system' | 'umd' | undefined
   target: string
   output: string
   banner: string
   name: string
+  declarationDir?: string | null | false
 }
-export default ({ input, format, target, output, banner, name }: IOptions) => {
+export default ({ input, format, target, output, banner, name, outDir, declarationDir }: IOptions) => {
   // acorn, acornInjectPlugins, cache, chunkGroupingSize, context, experimentalCacheExpiry, experimentalOptimizeChunks, experimentalTopLevelAwait, external, inlineDynamicImports, input, manualChunks, moduleContext, onwarn, perf, plugins, preserveModules, preserveSymlinks, shimMissingExports, strictDeprecations, treeshake, watch
+  if (declarationDir === undefined) {
+    declarationDir = `./${outDir}/typings`
+  }
+  declarationDir = declarationDir || ''
   return {
     inputOptions: {
       input,
@@ -23,13 +30,16 @@ export default ({ input, format, target, output, banner, name }: IOptions) => {
           verbosity: 2,
           tsconfigOverride: {
             compilerOptions: {
-              declaration: true,
-              declarationDir: './dist/types',
+              declaration: !!declarationDir,
+              declarationDir,
               module: 'esnext',
               target
             }
           },
           useTsconfigDeclarationDir: true
+        }),
+        replace({
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         }),
 
         format === 'umd' && uglify()
